e44ec763137545932ebcf4c86c3dae06da59dc8d
C <cesc@onqlave.com>
2023-06-14 23:30:31 +0700

    feat: send create arx request to dp

--- source/onqlave/connection/client.cpp
+++ source/onqlave/connection/client.cpp
@@ -1,7 +1,7 @@
 #include <connection/client.h>
 #include <requests/requests.h>
 #include <curl/curl.h>
-
+#include <nlohmann/json.hpp>
 #include <iostream>
 
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* response) {
@@ -11,13 +11,38 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* res
 }
 
 int client::Post(std::string resource, OnqlaveRequest* body, std::map<std::string, std::string> headers) {
-  std::string response;
   CURL* curl= curl_easy_init();
+
+  nlohmann::json ex1 = nlohmann::json::parse(R"(
+{
+  "arx": {
+        "id": "cluster--111111111111111111",
+        "name": "arx-kkk",
+        "tenant_id": "tenant--Is2tYXUJOt8tjvVNt1Qa1",
+        "owner": "m.mahdiyeh@gmail.com",
+        "spend_limit": 10,
+        "purpose": "development",
+        "plan": "serverless",
+        "encryption_method": "aes-gcm-128",
+        "rotation_cycle": 2592000,
+        "provider": "gcp",
+        "master_key": "mk",
+        "wrapping_key_store":"wks",
+        "regions": ["au"]
+      }
+}
+)");
+
+  std::string bodystr = ex1.dump();
 
   if (curl) {
     curl_easy_setopt(curl, CURLOPT_URL, resource.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    curl_easy_setopt(curl, CURLOPT_POST, 1L);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, bodystr.c_str());
+
+    struct curl_slist* headers = nullptr;
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
     CURLcode res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
@@ -26,7 +51,5 @@ int client::Post(std::string resource, OnqlaveRequest* body, std::map<std::strin
 
     curl_easy_cleanup(curl);
   }
-
-  std::cout << "============= response:" << response << std::endl;
   return 0;
 }
--- source/onqlave/keymanager/key_manager_client.cpp
+++ source/onqlave/keymanager/key_manager_client.cpp
@@ -37,7 +37,7 @@ keyManager::FetchEncryptionKey() {
   std::string algorithm = "";
 
   OnqlaveRequest* body = new EncryptionOpenRequest();
-  conn->Post("https://example.com", body);
+  conn->Post("http://localhost:8090/dataplane/administration/arxs", body);
 
   return std::make_tuple(edk, dk, algorithm);
 }
